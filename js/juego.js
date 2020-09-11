85b3afad2465d867393e10e1846b5420d8b5522c
sirlocust <jua56yu@hotmail.com>
2020-09-11 16:06:10 -0500

    intentando crear la logica de los botones y juego

--- js/juego.js
+++ js/juego.js
@@ -1,27 +1,64 @@
-
-
-
 let deck = [];
-const typeCarts= ['C','D','H','S']
-const specialCarts = ['A','J','Q','K']
-
-const createDeck= () => {
-
-    for( let i =2 ; i<= 10 ; i++){
-        for( let typeCart of typeCarts){
-            deck.push(i+typeCart)
-        }
+const typeCarts = ["C", "D", "H", "S"];
+const specialCarts = ["A", "J", "Q", "K"];
+const playerDeck = document.getElementById("jugador-cartas");
+const createDeck = () => {
+  for (let i = 2; i <= 10; i++) {
+    for (let typeCart of typeCarts) {
+      deck.push(i + typeCart);
     }
+  }
 
-    for( let typeCart of typeCarts){
-        for(let specialCart of specialCarts){
-            deck.push(specialCart + typeCart)
-        }
+  for (let typeCart of typeCarts) {
+    for (let specialCart of specialCarts) {
+      deck.push(specialCart + typeCart);
     }
+  }
 
-    deck = _.shuffle(deck)
-}
+  deck = _.shuffle(deck);
+};
 
+const addCart = () => {
+  //    const newCart = document.createElement('img').setAttribute('class','carta');
+  if(deck.length >0){
 
-createDeck()
-console.table(deck)
+      const posCart = genereteCardPos();
+      const cart = deck[posCart];
+      deck.splice(posCart, 1);
+      playerDeck.innerHTML += `<img class="carta" src="/assets/cartas/${cart}.png" alt=""></img>`;
+      return;
+  }
+  console.log("no hay mas cartas")
+
+};
+
+const buttonGiveCart = () => {
+  const button = document.getElementById("giveCart");
+  button.addEventListener("click", () => {
+    addCart();
+  });
+};
+
+const newGame = () => {
+  const button = document.getElementById("newGame");
+  button.addEventListener("click", () => {
+    console.log("go");
+    document.querySelectorAll(".carta").forEach((e) => {
+      e.remove();
+    });
+    startGame();
+  });
+};
+
+const startGame = () => {
+  createDeck();
+  buttonGiveCart();
+};
+
+newGame();
+
+const genereteCardPos = () => {
+  return Math.floor(Math.random() * (deck.length - 0) + 0);
+};
+
+console.table(deck);
