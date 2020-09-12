6c4e289cf8f9639cc5f0b475647f156f9243f96e
sirlocust <jua56yu@hotmail.com>
2020-09-11 20:14:43 -0500

    se agrega la logica del ganador y se organiza un poco el codigo java script

--- js/juego.js
+++ js/juego.js
@@ -11,6 +11,11 @@ const uiScorePlayer = document.querySelector("#playerScore")
 const uiScoreComputer= document.querySelectorAll('small')[1]
 const buttonstopGiveCarts = document.querySelector('#stopCarts')
 let  stopGiveCarts = false
+
+
+
+// modulos de creacion de cartas
+
 const createDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let typeCart of typeCarts) {
@@ -27,6 +32,25 @@ const createDeck = () => {
   //   deck = _.shuffle(deck);
 };
 
+////////////////////////////////////////////
+
+// botones listeners
+buttonstopGiveCarts.addEventListener('click', () => {
+  callTurnComputer()
+})
+
+buttonGiveCart.addEventListener("click", () => {
+  const cart = giveCart()   
+    addCart(cart, whichPlayerGame);
+  });
+
+
+
+
+////////////////////////////////////////////
+
+
+/// logica del juego  y player
 const addCart = (cart, whichPlayer) => {
   //    const newCart = document.createElement('img').setAttribute('class','carta');
 
@@ -36,7 +60,6 @@ const addCart = (cart, whichPlayer) => {
 
       playerDeck.innerHTML += `<img class="carta" src="/assets/cartas/${cart}.png" alt=""></img>`;
       if(scorePlayer > 21  ){
-        alert('perdieste')
         // buttonGiveCart.setAttribute('disabled','')
        callTurnComputer()
       }
@@ -52,16 +75,6 @@ const addCart = (cart, whichPlayer) => {
   }
 };
 
-buttonstopGiveCarts.addEventListener('click', () => {
-  callTurnComputer()
-})
-
-const  callTurnComputer = () => {
-  whichPlayerGame = 'computer'
-  gameComputer()
-  buttonGiveCart.toggleAttribute('disabled')
-} 
-
 const giveCart = () => {
   const posCart = genereteCardPos();
   const cart = deck[posCart];
@@ -69,10 +82,71 @@ const giveCart = () => {
   return cart;
 };
 
-buttonGiveCart.addEventListener("click", () => {
-  const cart = giveCart()   
-  addCart(cart, whichPlayerGame);
-});
+
+const valueCart = (cart , whichPlayer) => {
+  const value = cart.substring(0, cart.length - 1);
+
+  if(whichPlayer === "player"){
+
+    scorePlayer += !isNaN(value) ? parseInt(value, 10) : value === "A" ? 11 : 10;
+    refreshScorePlayer(scorePlayer , whichPlayer);
+    return
+  }
+  if(whichPlayer === 'computer'){
+    scoreComputer += !isNaN(value) ? parseInt(value, 10) : value === "A" ? 11 : 10;
+    refreshScorePlayer(scoreComputer, whichPlayer);
+    return
+  }
+
+};
+
+
+const refreshScorePlayer = (score, whichPlayer) => {
+ 
+  if(whichPlayer === "player"){
+
+    uiScorePlayer.textContent = score;
+    return
+  }
+  if(whichPlayer === 'computer'){
+    uiScoreComputer.textContent = score
+    return
+  }
+};
+
+const genereteCardPos = () => {
+  return Math.floor(Math.random() * (deck.length - 0) + 0);
+};
+
+////////////////////////////////////////////////////////
+
+/// logica computadora 
+
+const  callTurnComputer = () => {
+  whichPlayerGame = 'computer'
+  gameComputer()
+  buttonGiveCart.toggleAttribute('disabled')
+} 
+
+
+const gameComputer =() =>{
+  while(scoreComputer <= scorePlayer && scoreComputer <= 21){
+    const cart = giveCart() 
+    addCart(cart, whichPlayerGame)
+    if(scorePlayer>21){
+      break
+    }
+  }
+  const resultGame = (scorePlayer > 21 )?"gano computadora":
+                    (scorePlayer > scoreComputer )? "gano player": 
+                    (scoreComputer <= 21 )?"gano computadoraz":"gano Player"
+  alert(resultGame)
+}
+///////////////////
+
+
+
+/// logica del inicio del juego
 
 const newGame = () => {
   const button = document.getElementById("newGame");
@@ -92,49 +166,11 @@ const newGame = () => {
   });
 };
 
-const valueCart = (cart , whichPlayer) => {
-  const value = cart.substring(0, cart.length - 1);
-
-  if(whichPlayer === "player"){
-
-    scorePlayer += !isNaN(value) ? parseInt(value, 10) : value === "A" ? 11 : 10;
-    refreshScorePlayer(scorePlayer , whichPlayer);
-    return
-  }
-  if(whichPlayer === 'computer'){
-    scoreComputer += !isNaN(value) ? parseInt(value, 10) : value === "A" ? 11 : 10;
-    refreshScorePlayer(scoreComputer, whichPlayer);
-    return
-  }
-
-};
-
-const refreshScorePlayer = (score, whichPlayer) => {
- 
-  if(whichPlayer === "player"){
-
-    uiScorePlayer.textContent = score;
-    return
-  }
-  if(whichPlayer === 'computer'){
-    uiScoreComputer.textContent = score
-    return
-  }
-};
-
 const startGame = () => {
   createDeck();
 };
 
-const genereteCardPos = () => {
-  return Math.floor(Math.random() * (deck.length - 0) + 0);
-};
 
-const gameComputer =() =>{
-  while(scoreComputer <= scorePlayer && scoreComputer <= 21){
-    const cart = giveCart() 
-    addCart(cart, whichPlayerGame)
-  }
-}
+///////////////////////////////
 
 newGame();
